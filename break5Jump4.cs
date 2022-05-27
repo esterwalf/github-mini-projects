58646adb23f7577daba027baf9039e7e140e76fd
esterwalf <66186219+esterwalf@users.noreply.github.com>
2022-05-27 23:55:57 +0100

    Create break5Jump4.cs

---
+++ break5Jump4.cs
@@ -0,0 +1,28 @@
+Using System;
+namespace My Application
+{
+class Program
+{
+    Static void Main (string[]args)
+{
+  for (int i=0; i<5;i++)
+  {
+      if (i==5)
+      {
+          break;
+      }
+  }
+  Console.WriteLine(i)
+}
+{
+    for (int x=0; i<10;i++)
+    {
+        if (==4)
+        {
+            continue;
+        }
+        Console.WriteLine(x);
+    }
+}
+}
+}
